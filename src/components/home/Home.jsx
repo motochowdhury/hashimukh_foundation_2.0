8180adbe7a3a29aaeb31b18b4b5aaf7777293db9
motochowdhury <motochowdhury@gmail.com>
2023-01-14 16:22:41 +0600

    Home section Layout Setup done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
       <div className="relative">
         <div className="bg-blue-600 h-[600px] absolute w-10 rounded-t-full left-32 opacity-30 rounded-b-full rotate-12 lg:-rotate-45 blur-2xl"></div>
         <div className="absolute top-32 bg-orange-500 opacity-30 h-[600px] w-20 rounded-t-full rounded-b-full rotate-12 lg:-rotate-45 blur-xl"></div>
+        <div className="absolute inset-0">
+          <div className="grid grid-cols-1 lg:grid-cols-2 pt-32"></div>
+        </div>
       </div>
     </section>
   );
