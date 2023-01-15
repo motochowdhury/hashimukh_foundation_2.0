d26dbcc67a17befa744718d5eabb738593faee28
motochowdhury <motochowdhury@gmail.com>
2023-01-15 20:11:57 +0600

    Navbar responsive design under development with logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,29 +2,28 @@ import React from "react";
 import logo from "../../assets/hmk__logo_small.png";
 
 const Header = () => {
+  let navItems = [
+    { link: "#home", name: "হোম" },
+    { link: "#home", name: "ব্লাড_খুঁজুন" },
+    { link: "#home", name: "এডমিনেট্রেশন" },
+    { link: "#home", name: "যোগাযোগ করুন" },
+  ];
   return (
     <section>
-      <nav className="flex items-center lg:justify-between">
-        <div className="flex items-center">
-          <img className="w-[100px] h-[100px]" src={logo} alt="" />
-          <h1 className="text-green-500 text-3xl font-sandwip">
-            হাঁসি মুখ ফাউন্ডেশন
-          </h1>
+      <nav className="flex flex-col lg:flex-row items-center lg:justify-between bg-white/0 backdrop-blur-xl">
+        <div className="flex items-center justify-around w-full lg:w-auto">
+          <div className="flex items-center">
+            <img
+              className="lg:w-[100px] lg:h-[100px] w-20 h-20"
+              src={logo}
+              alt=""
+            />
+            <h1 className="text-green-500 text-2xl lg:text-3xl font-sandwip">
+              হাঁসি মুখ ফাউন্ডেশন
+            </h1>
+          </div>
+          <div className="lg:hidden text-white">close</div>
         </div>
-        <ul className="flex space-x-4 font-sandwip text-gray-500">
-          <li className="hover:text-white hover:transition-all duration-300">
-            <a href="">হোম</a>
-          </li>
-          <li className="hover:text-white hover:transition-all duration-300">
-            <a href="">ব্লাড_খুঁজুন</a>
-          </li>
-          <li className="hover:text-white hover:transition-all duration-300">
-            <a href="">এডমিনেট্রেশন</a>
-          </li>
-          <li className="hover:text-white hover:transition-all duration-300">
-            <a href="">যোগাযোগ করুন</a>
-          </li>
-        </ul>
       </nav>
     </section>
   );
