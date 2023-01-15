385e703a9cf79cc5e3767b8eca6fb1d6d2e902fb
motochowdhury <motochowdhury@gmail.com>
2023-01-15 20:31:12 +0600

    navbar nav icon added and made navbar functional

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/hmk__logo_small.png";
 
 const Header = () => {
@@ -8,8 +8,9 @@ const Header = () => {
     { link: "#home", name: "এডমিনেট্রেশন" },
     { link: "#home", name: "যোগাযোগ করুন" },
   ];
+  const [open, setOpen] = useState(false);
   return (
-    <section>
+    <section className="">
       <nav className="flex flex-col lg:flex-row items-center lg:justify-between bg-white/0 backdrop-blur-xl">
         <div className="flex items-center justify-around w-full lg:w-auto">
           <div className="flex items-center">
@@ -22,9 +23,35 @@ const Header = () => {
               হাঁসি মুখ ফাউন্ডেশন
             </h1>
           </div>
-          <div className="lg:hidden text-white">close</div>
+          <div onClick={() => setOpen(!open)} className="lg:hidden text-white">
+            {open ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                style={{ fill: "#ffffff" }}
+              >
+                <path d="M5 6a.4.4 0 0 0-.38.62l2 3.38-3.06 5.4a.45.45 0 0 0 0 .45.43.43 0 0 0 .38.19h2.87a.89.89 0 0 0 .79-.55s3-5.31 3.11-5.51l-2-3.46A.91.91 0 0 0 7.92 6zm12.16-4a.84.84 0 0 0-.77.55L10 13.93l4.09 7.52a.91.91 0 0 0 .81.55h2.88a.43.43 0 0 0 .38-.18.45.45 0 0 0 0-.45l-4.07-7.43 6.36-11.31a.45.45 0 0 0 0-.45.44.44 0 0 0-.38-.18z"></path>
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                style={{ fill: "#ffffff" }}
+              >
+                <path d="M21.063 15H13v2h9v-2zM4 7h11v2H4zm0 4h11v2H4zm0 4h7v2H4z"></path>
+              </svg>
+            )}
+          </div>
         </div>
-        <ul className="flex flex-col lg:flex-row space-x-4 font-sandwip text-gray-500">
+        <ul
+          className={`static flex flex-col lg:flex-row space-x-4 font-sandwip text-gray-500 ${
+            open ? "flex" : "hidden"
+          } transition-all duration-500 ease-in`}
+        >
           {navItems.map((link) => (
             <li
               key={link.name}
