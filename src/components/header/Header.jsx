9e5e45d162da93d66ab54a222a4fdb196aeb4a4f
motochowdhury <motochowdhury@gmail.com>
2023-01-15 12:00:18 +0600

    nav item styling is in under development

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import logo from "../../assets/hmk__logo_small.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section>
+      <nav className="flex items-center lg:justify-between">
+        <div className="flex items-center">
+          <img className="w-[100px] h-[100px]" src={logo} alt="" />
+          <h1 className="text-green-500 text-3xl font-sandwip">
+            হাঁসি মুখ ফাউন্ডেশন
+          </h1>
+        </div>
+        <ul className="flex space-x-4 font-sandwip text-gray-500">
+          <li className="hover:text-white">
+            <a href="">হোম</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="">ব্লাড_খুঁজুন</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="">এডমিনেট্রেশন</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="">যোগাযোগ করুন</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 };
 
 export default Header;
