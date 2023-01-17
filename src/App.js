c0ad00baa1eee30433b3cea30e1d01e76f711436
motochowdhury <motochowdhury@gmail.com>
2023-01-17 18:05:58 +0600

    Setting up Search donor bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import SearchBarHome from "./components/searchBarHome/SearchBarHome";
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <SearchBarHome />
     </>
   );
 };
---
+++ src/components/searchBarHome/SearchBarHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBarHome = () => {
+  return (
+    <section className="">
+      <h1 className="text-white text-5xl">MotoC</h1>
+    </section>
+  );
+};
+
+export default SearchBarHome;
