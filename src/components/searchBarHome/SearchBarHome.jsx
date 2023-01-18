8ac0d8a44e2c146f0060f2cbba90e03884fa2f98
motochowdhury <motochowdhury@gmail.com>
2023-01-18 21:39:27 +0600

    Responsive find donor image

--- src/components/searchBarHome/SearchBarHome.jsx
+++ src/components/searchBarHome/SearchBarHome.jsx
@@ -5,7 +5,7 @@ const SearchBarHome = () => {
     <section>
       <div className="h-screen mt-10 px-10 text-center">
         <div className="py-10">
-          <img className="w-40 mx-auto mb-5" src={assets} alt="" />
+          <img className="w-32 lg:w-40 mx-auto mb-5" src={assets} alt="" />
           <h1 className="text-gray-200 text-2xl md:text-3xl lg:text-4xl font-sandwip">
             রক্তদাতা <span className="text-blue-400">খুঁজুন</span>
           </h1>
