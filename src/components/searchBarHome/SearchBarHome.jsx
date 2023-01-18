37e0997802114d4e4123695ea102daca43034ca7
motochowdhury <motochowdhury@gmail.com>
2023-01-18 21:38:18 +0600

    Responsive title for find donor is done

--- src/components/searchBarHome/SearchBarHome.jsx
+++ src/components/searchBarHome/SearchBarHome.jsx
@@ -6,7 +6,7 @@ const SearchBarHome = () => {
       <div className="h-screen mt-10 px-10 text-center">
         <div className="py-10">
           <img className="w-40 mx-auto mb-5" src={assets} alt="" />
-          <h1 className="text-gray-200 text-4xl font-sandwip">
+          <h1 className="text-gray-200 text-2xl md:text-3xl lg:text-4xl font-sandwip">
             রক্তদাতা <span className="text-blue-400">খুঁজুন</span>
           </h1>
         </div>
