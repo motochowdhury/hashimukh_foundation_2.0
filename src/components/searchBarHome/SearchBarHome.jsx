164216a35b9113896c845ced84bd1642af9419d0
motochowdhury <motochowdhury@gmail.com>
2023-01-19 19:12:28 +0600

    Added toggle function added

--- src/components/searchBarHome/SearchBarHome.jsx
+++ src/components/searchBarHome/SearchBarHome.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../../assets/Asset 1.svg";
 const SearchBarHome = () => {
+  const [toggle, setSearchToggle] = useState(false);
   return (
     <section>
       <div className="h-screen mt-10 px-10 text-center">
@@ -12,8 +13,16 @@ const SearchBarHome = () => {
         </div>
         <div className="flex justify-center">
           <div className="relative">
-            <div className="bg-gradient-to-r from-pink-900 to-green-700 blur-xl w-[500px] h-[50px] rounded-md border border-1 border-gray-800"></div>
-            <div className="absolute inset-0 bg-bg-color w-[500px] h-[50px] rounded-md border border-1 border-gray-800"></div>
+            <div className="bg-gradient-to-r from-pink-900 to-green-700 blur-2xl w-[500px] h-[50px] rounded-md border border-1 border-gray-800 opacity-75"></div>
+            <div
+              onClick={() => setSearchToggle(!toggle)}
+              className="absolute inset-0 bg-bg-color w-[500px] h-[50px] rounded-md border border-1 border-gray-800"
+            ></div>
+            <div
+              className={`bg-white h-72 w-80 transition-all duration-1000 ease-out ${
+                toggle ? "block" : "hidden"
+              }`}
+            ></div>
           </div>
         </div>
       </div>
