8df7a41b6df647b62371a7574ada9e3a5b2e41ff
motochowdhury <motochowdhury@gmail.com>
2023-02-03 22:52:27 +0600

    Remove test item

--- src/components/relationship/RelationShip.jsx
+++ src/components/relationship/RelationShip.jsx
@@ -3,11 +3,7 @@ import React from "react";
 export const RelationShip = () => {
   return (
     <div>
-      <div>Md Jishan</div>
-      <div>5-04-1999</div>
-      <div>01837783623</div>
-      <div>jmd187421@gmail.com</div>
-      <div>https//:facebook.com/Mishal.FF</div>
+      
     </div>
   );
 };
